f14a22289f95ed4c0e8c1a1994d87b8fdcdabe39
Wallace Vilela <wallace.vilela9@gmail.com>
2023-05-03 19:39:11 -0300

    Digital clock implementation

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 let digitalElement = document.querySelector('.digital');
-let sElement = document.querySelector('p_s');
-let mElement = document.querySelector('p_m');
-let hElement = document.querySelector('p_h');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
+
+function UpdateClock() {
+    //FUNCIONAMENTO RELOGIO DIGITAL
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+    digitalElement.innerHTML = `${FixZero(hour)}:${FixZero(minute)}:${FixZero(second)}`;
+
+
+
+    //FUNCIOMENTO RELOGIO ANALOGICO
+    let sDeg = (360 / 60 * second) - 90;
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+}
+
+function FixZero(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(UpdateClock, 1000);
